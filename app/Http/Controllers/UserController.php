c3f57f408372ed61f23f0257d45b0516300ccb43
InggaChintia <inggainggachintia@gmail.com>
2024-09-18 09:43:16 +0700

    js4 praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,23 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //JS 4 PRAKTIKUM 1
+    //JS 4 
     public function index() {
+        //PRAKTIKUM 2.2
+        //UPDATE
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+
         //PRAKTIKUM 2.1
         //UPDATE 4
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
         //UPDATE 3
         // $user = UserModel::findOr(1, ['username', 'nama'], function () {
