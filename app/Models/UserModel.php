54a070651762641aa9ac7eb3d3cbd5eb455198dd
InggaChintia <inggainggachintia@gmail.com>
2024-09-20 14:23:59 +0700

    js4 Praktikum 2.7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -19,7 +20,12 @@ class UserModel extends Model
      */
     
     // JS 4 PRAKTIKUM 1
-    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    //protected $fillable = ['level_id', 'username', 'nama', 'password'];
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    //JS 4 PRAKTIKUM 2.7
+    public function level(): BelongsTo {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 
 }
